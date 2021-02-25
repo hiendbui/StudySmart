ee644135d603d82c4326e23ff1915bde4adba4f3
hiendbui <hiendbui0@gmail.com>
2021-02-25 01:25:43 -0800

    Set up users and quizzes routes

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require("./config/keys").mongoURI;
-
+const users = require("./routes/api/users");
+const quizzes = require("./routes/api/quizzes");
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
-    
+
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+app.use("/api/users", users);
+app.use("/api/quizzes", quizzes);
 
 const port = process.env.PORT || 5000;
 
