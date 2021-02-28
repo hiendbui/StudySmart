678e9094b2cf4ca5da98b738ef07dafffaf31968
hiendbui <hiendbui0@gmail.com>
2021-02-28 14:47:55 -0800

    Add login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
 const bcrypt = require("bcryptjs");
+const passport = require('passport');
 
 router.post('/register', (req, res) => {
     User.findOne({ username: req.body.username })
@@ -26,5 +27,26 @@ router.post('/register', (req, res) => {
             })
         }  
     })
+})
+
+router.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    User.find({ username })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ username: "This user does not exist."});
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: "Success" });
+                    } else {
+                        return res.status(404).json({password: "Incorrect password"});
+                    }
+                })
+        })
 })
 module.exports = router;
