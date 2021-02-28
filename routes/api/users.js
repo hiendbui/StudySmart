185d77e7843b37d638ea4451c266000c8e3ca681
hiendbui <hiendbui0@gmail.com>
2021-02-28 15:04:15 -0800

    Add and edit user routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,21 @@ const User = require('../../models/User');
 const bcrypt = require("bcryptjs");
 const passport = require('passport');
 
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    username: req.user.username,
+    type: req.user.username
+  });
+})
+
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ username: req.body.username })
     .then(user => {
         if (user) {
@@ -21,7 +35,16 @@ router.post('/register', (req, res) => {
                     if (err) throw err;
                     newUser.password = hash;
                     newUser.save()
-                        .then(user => res.json(user))
+                        .then(user => {
+                            const payload = { id: user.id, handle: user.handle };
+                            //key expires in 1 hour
+                            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            });
+                        })
                         .catch(err => console.log(err))
                 })
             })
@@ -30,6 +53,12 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    
     const username = req.body.username;
     const password = req.body.password;
     
@@ -42,11 +71,20 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({ msg: "Success" });
+                        const payload = { id: user.id, handle: user.handle };
+
+                        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                            res.json({
+                                success: true,
+                                token: "Bearer " + token
+                            });
+                        });
                     } else {
-                        return res.status(404).json({password: "Incorrect password"});
+                        errors.password = "Incorrect password";
+                        return res.status(400).json(errors);
                     }
-                })
-        })
-})
+                });
+        });
+});
+
 module.exports = router;
