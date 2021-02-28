3aeae85de7d9494a3a9b40670d427286d8272aff
hiendbui <hiendbui0@gmail.com>
2021-02-28 14:33:21 -0800

    set up bcrypt for password

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ const mongoose = require('mongoose');
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const quizzes = require("./routes/api/quizzes");
+const User = require('./models/User');
+const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
+app.use(bodyParser.urlencoded({ 
+    extended: false
+}));
+
+// app.get('/', (req, res) => {
+//     const user = new User({
+//         username: 'test',
+//         password: 'password',
+//         type: 'instructor'
+//     })
+//     user.save();
+//     res.send("Hello World!");
+// });
 
 app.use("/api/users", users);
 app.use("/api/quizzes", quizzes);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const bcrypt = require("bcryptjs");
 
-router.get("/test", (req, res) => {
-    res.json({ msg: "This is the user route" });
-});
+router.post('/register', (req, res) => {
+    User.findOne({ username: req.body.username })
+    .then(user => {
+        if (user) {
+            return res.status(400).json({username: "Username already taken"})
+        } else {
+            const newUser = new User({
+                username: req.body.username,
+                password: req.body.email,
+                type: req.body.type
+            })
 
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err))
+                })
+            })
+        }  
+    })
+})
 module.exports = router;
