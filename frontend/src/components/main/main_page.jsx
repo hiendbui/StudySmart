a755c6c9a14fcfe220d3375dc963a53c3153fc7f
hiendbui <hiendbui0@gmail.com>
2021-03-01 01:46:14 -0800

    Add main and nav

---
+++ frontend/src/components/main/main_page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>StudySmart</h1>
+        <footer>
+          Created by Hien Bui &copy; 2021
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
