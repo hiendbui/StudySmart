4db6832acc1bdcc73cf4c2743f5f9f7429094126
hiendbui <hiendbui0@gmail.com>
2021-03-03 02:48:05 -0800

    Refactor login form w/ hooks

--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -1,85 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../../actions/session_actions';
 
-class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      username: '',
-      password: '',
-      errors: {}
-    };
+function LoginForm() {
+  const errors = useSelector(state => state.errors.session);
+  const dispatch = useDispatch();
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.renderErrors = this.renderErrors.bind(this);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.currentUser === true) {
-      this.props.history.push('/home');
-    }
-
-    // Set or clear errors
-    this.setState({errors: nextProps.errors})
-  }
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
 
   // Handle field updates (called in the render method)
-  update(field) {
-    return e => this.setState({
-      [field]: e.currentTarget.value
-    });
+  function update(field) {
+    return e => setFormData({...formData, [field]: e.target.value})
   }
 
   // Handle form submission
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-
-    let user = {
-      username: this.state.username,
-      password: this.state.password
-    };
-
-    this.props.login(user); 
+    dispatch(login(formData));
   }
 
   // Render the session errors if there are any
-  renderErrors() {
+  function renderErrors() {
     return(
       <ul>
-        {Object.keys(this.state.errors).map((error, i) => (
+        {Object.keys(errors).map((error, i) => (
           <li key={`error-${i}`}>
-            {this.state.errors[error]}
+            {errors[error]}
           </li>
         ))}
       </ul>
     );
   }
 
-  render() {
+  
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <div>
               <input type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
+                value={formData.username}
+                onChange={update('username')}
                 placeholder="Username"
               />
             <br/>
               <input type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
+                value={formData.password}
+                onChange={update('password')}
                 placeholder="Password"
               />
             <br/>
             <input type="submit" value="Submit" />
-            {this.renderErrors()}
+            {renderErrors()}
           </div>
         </form>
       </div>
     );
-  }
+  
 }
 
-export default withRouter(LoginForm);
+export default LoginForm;
