eb69c5fbc57e72b703b5855a9be2bddedcf371ed
hiendbui <hiendbui0@gmail.com>
2021-03-03 02:37:07 -0800

    Add clearErrors to navbar click handler

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useHistory } from "react-router";
 import { logout } from '../../actions/session_actions';
-
+import { clearErrors } from '../../actions/session_actions'
 
 const NavBar = () => {
-
   const loggedIn = useSelector(state => state.session.isAuthenticated);
   const dispatch = useDispatch();
+  const history = useHistory();
 
+  function handleClick(field) {
+    dispatch(clearErrors());
+    history.push(`./${field}`)
+  }
   // Selectively render links dependent on whether the user is logged in
   function getLinks() {
       if (loggedIn) {
@@ -23,8 +27,8 @@ const NavBar = () => {
       } else {
         return (
             <div>
-                <Link to={'/signup'}>Signup</Link>
-                <Link to={'/login'}>Login</Link>
+                <button onClick={() => handleClick('signup')}>Signup</button>
+                <button onClick={() => handleClick('login')}>Login</button>
             </div>
         );
       }
