a1b9bdeee8cf4135bcccb8a1927937df13dccfc2
hiendbui <hiendbui0@gmail.com>
2021-03-10 15:58:15 -0800

    Create flashcard model

---
+++ models/Flashcard.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FlashcardSchema = new Schema({
+    quiz: {
+        type: Schema.Types.ObjectId,
+        ref: 'Quiz'
+    },
+    front: {
+        type: String,
+        required: true
+    },
+    back: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        required: true
+    }
+}, {
+  timestamps: true
+})
+
+const Flashcard = mongoose.model('Flashcard', FlashcardSchema);
+module.exports = Flashcard;
