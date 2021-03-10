efe953670da0aaf84189e8050cd7b700fe7a0842
hiendbui <hiendbui0@gmail.com>
2021-03-09 22:50:12 -0800

    Add patch and delete routes

--- routes/api/quizzes.js
+++ routes/api/quizzes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const Quiz = require('../../models/Quiz');
 const validateQuizInput = require('../../validation/quizzes');
 
@@ -24,6 +25,41 @@ router.post('/',
       });
       
       newQuiz.save().then(quiz => res.json(quiz));
+    }
+);
+
+router.delete('/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+
+        Quiz.deleteOne({_id: req.params.id})
+            .then(() => {
+                res.json({ id: req.params.id, message: 'Deleted!' });
+            })
+            .catch(error => {
+                res.status(400).json({ error });
+            });
+    }
+);
+
+router.patch('/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validateQuizInput(req.body);
+    
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        Quiz.findById(req.params.id)
+            .then(quiz => {
+                quiz.topic = req.body.topic;
+                quiz.description = req.body.description;
+                quiz.save().then(quiz => res.json(quiz));
+            })
+            .catch(err =>
+                res.status(404).json({ noquizzesfound: 'No quiz found with that ID' })
+            )
     }
 );
 
