922f300f318351d282730c877c58431b8003aaca
hiendbui <hiendbui0@gmail.com>
2021-03-09 19:49:08 -0800

    Add get route for quizzes

--- routes/api/quizzes.js
+++ routes/api/quizzes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../../models/Quiz');
 
-router.get("/test", (req, res) => {
-    res.json({ msg: "This is the quiz route" });
+router.get("/quizzes", (req, res) => {
+    Quiz.find()
+        .then(quizzes => res.json(quizzes))
+        .catch(err => res.status(404).json({ noquizzesfound: 'No quizzes found' }));
 });
 
 module.exports = router;
