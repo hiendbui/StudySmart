66e9a2beac8896b4602b4df99e9670dbab826d05
hiendbui <hiendbui0@gmail.com>
2021-03-10 21:47:40 -0800

    Add fetch quiz req

--- frontend/src/util/quiz_api_util.js
+++ frontend/src/util/quiz_api_util.js
@@ -4,13 +4,13 @@ export const fetchQuizzes = () => {
     return axios.get('api/quizzes')
 };
 
+export const fetchQuiz = id => {
+    return axios.get(`api/quizzes/${id}`)
+};
+
 export const createQuiz = quiz => {
     return axios.post('api/quizzes', quiz)
 };
-
-// export const fetchQuiz = id => {
-//     return axios.get(`api/quizzes/${id}`)
-// };
 
 export const deleteQuiz = id => {
     return axios.delete(`api/quizzes/${id}`);
