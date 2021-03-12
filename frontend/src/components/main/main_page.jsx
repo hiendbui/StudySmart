0e21d1746885b25b37459db61ad8db942a7e4752
hiendbui <hiendbui0@gmail.com>
2021-03-11 18:40:15 -0800

    Add react icon

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllQuizzes } from '../../actions/quiz_actions';
 import QuizItem from './quiz_item';
+import { MdAddToPhotos } from 'react-icons/md'
 import './main.scss';
 
 function MainPage() {
@@ -14,6 +15,7 @@ function MainPage() {
       <div className='main'>
         <div className='quiz-container'>
           <h1>Flashcard Quizzes</h1>
+          <MdAddToPhotos className='add-icon'/>
           <div className='quizzes'>
             {quizzes.map((quiz)=>{
               return (
