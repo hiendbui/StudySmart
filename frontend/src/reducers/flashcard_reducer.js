44c35a23b4c33bdb2fea36ea1b61c37b0b565f2f
hiendbui <hiendbui0@gmail.com>
2021-03-12 17:02:43 -0800

    Add flashcard actions to reducer

--- frontend/src/reducers/flashcard_reducer.js
+++ frontend/src/reducers/flashcard_reducer.js
@@ -2,9 +2,14 @@ import {
   RECEIVE_QUIZ,
   REMOVE_QUIZ
 } from '../actions/quiz_actions';
+import { 
+  RECEIVE_FLASHCARD,
+  REMOVE_FLASHCARD
+} from '../actions/flashcard_actions';
 
 const FlashcardsReducer = (state = {}, action) => {
   Object.freeze(state);
+  const newState = {...state}
   switch(action.type) {
     case RECEIVE_QUIZ:
         if (!action.flashcards) return state;
@@ -13,6 +18,12 @@ const FlashcardsReducer = (state = {}, action) => {
         return cardsObj;
     case REMOVE_QUIZ:
         return {};
+    case RECEIVE_FLASHCARD:
+        const flashcard = {[action.flashcard._id]: action.flashcard}
+        return {...newState, ...flashcard};
+    case REMOVE_FLASHCARD:
+        delete newState[action.flashcardId];
+        return newState;
     default:
       return state;
   }
